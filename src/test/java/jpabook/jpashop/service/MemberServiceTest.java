03a9de6d3a2e6a654ea62fb6bcd2b0da919f3c2b
leeso <leesub0168@gmail.com>
2023-05-29 00:14:01 +0900

    회원 기능 테스트 & 스프링 부트 테스트 메모리 db 설정

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest { // 테스트는 완전히 격리된 환경으로 진행하는게 좋음.
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        /**
+         * 테스트라서 롤백 되기 때문에 insert 쿼리가 나가지 않음.
+         * 쿼리를 확인하고 싶다면 flush나 rollback-false 세팅하면됨
+         * em.flush() , @Rollback(value = false)
+         */
+         assertEquals(member, memberRepository.findOne(saveId));
+    }
+    
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        //then
+//        fail("예외가 발생해야 한다.");
+    }
+}
