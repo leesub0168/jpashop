336de0f47df754f5b84e12b5f75ed6c3dca64f8b
leeso <leesub0168@gmail.com>
2023-06-01 19:28:41 +0900

    상품 주문 & 주문 내역

---
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String create(@RequestParam("memberId") Long memberId,
+                         @RequestParam("itemId") Long itemId,
+                         @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        /**
+         * 핵심로직같은 경우에는 엔티티를 생성해서 넘기는 것보다, 인자값만 넘겨서
+         * 서비스단에서 비즈니스 로직을 수행하고 엔티티를 업데이트 하는게 
+         * 더 효율적이다. 변경 감지가 용이하기 때문
+         * */
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
+}
